c76ed6f4b94d0d3f21223d933944df6b245dd534
esteves-esta <26911877+esteves-esta@users.noreply.github.com>
2023-12-12 22:09:11 -0300

    test idea

--- app/src/components/FeedProvider/FeedProvider.jsx
+++ app/src/components/FeedProvider/FeedProvider.jsx
@@ -14,30 +14,76 @@ https://thewebdev.info/2021/02/13/create-an-rss-reader-with-react-and-javascript
 https://humansofseoul.com/rss
 */
 
-async function fetcher(oi) {
-  console.log(oi)
+async function fetcher(teste) {
+  console.log(teste);
   const response = await fetch(FEED_URL, {
     method: "GET",
   });
   const text = await response.text();
-  const dataXML = new window.DOMParser().parseFromString(text, "text/xml");
+  const dataXML = new DOMParser().parseFromString(text, "text/xml");
 
   const itemsHTML = dataXML.querySelectorAll("item");
-  const posts = [];
-  console.log("oi");
-  itemsHTML.forEach((el) => {
-    const content = el.querySelector("description")?.innerHTML;
-    // console.log(content);
-    posts.push({
-      title: el.querySelector("title")?.innerHTML || "",
-      link: el.querySelector("link")?.innerHTML || "",
-      pubDate: el.querySelector("pubDate")?.innerHTML || "",
-      kor: "",
-      eng: "",
-    });
+  const posts = parseDOM2JSON(itemsHTML);
+  posts.map((item) => {
+    item.korSplit = item.kor.split(". ");
+    item.engSplit = item.eng.replace(/([a-zA-Z]{1})\.([a-zA-Z]{1})\./g, '$1$2').split(". ");
+    // console.log(item)
   });
-  console.log({ posts });
-  return {posts};
+  return { posts };
+}
+
+function parseDOM2JSON(items) {
+  const posts = [];
+  items.forEach((el) => {
+    const descriptionTag = el.querySelector("description");
+
+    // from: https://stackoverflow.com/questions/10585029/parse-an-html-string-with-js
+    const tempDOM = document.createElement("html");
+    tempDOM.innerHTML = `<body>${descriptionTag.textContent}</body>`;
+
+    const paragraphs = tempDOM.querySelectorAll("p");
+    let indexKor = 0;
+    let indexEng = 1;
+
+    if (paragraphs.length > 2) {
+      if (paragraphs[0].childNodes[0].nodeName === "BR") {
+        indexKor = 1;
+        indexEng = 2;
+        if (
+          paragraphs[2].childNodes[0].textContent.includes("Humans of Seoul")
+        ) {
+          indexEng = 3;
+        }
+      }
+      if (paragraphs[1].childNodes[0].textContent.includes("Humans of Seoul")) {
+        indexEng = 2;
+      }
+    }
+    const tempDOM2 = document.createElement("html");
+    tempDOM2.innerHTML = `<head><meta charset="UTF-8" /></head><body>${
+      el.querySelector("title")?.textContent
+    }</body>`;
+
+    if (
+      !paragraphs[indexKor]?.textContent.includes("Humans of Seoul") &&
+      !paragraphs[indexEng]?.textContent.includes("Humans of Seoul")
+    ) {
+      posts.push({
+        title: tempDOM2?.textContent || "",
+        link: el.querySelector("link")?.innerHTML || "",
+        pubDate: el.querySelector("pubDate")?.innerHTML || "",
+        kor: paragraphs[indexKor]?.textContent,
+        eng: paragraphs[indexEng]?.textContent,
+      });
+    } /* else {
+      console.log(indexKor);
+      console.log(paragraphs[indexKor]?.textContent);
+      console.log(indexEng);
+      console.log(paragraphs[indexEng]?.textContent);
+      console.log("ooo");
+    } */
+  });
+  return posts;
 }
 
 const errorRetry = (error, key, config, revalidate, { retryCount }) => {
@@ -53,14 +99,13 @@ const errorRetry = (error, key, config, revalidate, { retryCount }) => {
 export const FeedContext = React.createContext();
 
 function FeedProvider({ children }) {
-  const { data, error, isLoading } = useSWR("testes", fetcher);
-  
+  const { data, error, isLoading } = useSWR("testes", fetcher, {
+    errorRetry,
+    // revalidateOnFocus: false,
+  });
+
   const state = { data, error, isLoading };
-  return (
-    <FeedContext.Provider value={state}>
-      {children}
-    </FeedContext.Provider>
-  );
+  return <FeedContext.Provider value={state}>{children}</FeedContext.Provider>;
 }
 
 export default FeedProvider;
--- app/src/components/Home/Home.tsx
+++ app/src/components/Home/Home.tsx
@@ -3,14 +3,54 @@ import { FeedContext } from '../FeedProvider'
 
 function Home() {
   const { data } = React.useContext(FeedContext)
+  const [teste, setTeste] = React.useState(null)
+
+  React.useEffect(() => {
+    if (data?.posts) setTeste(data.posts[0])
+  }, [data])
 
   return <div>
     <h1>oi</h1>
     <p>
       {data?.posts && data.posts?.length}
     </p>
-    {/* {data?.posts && data.posts} */}
+
+    {teste && <> <h3>{teste.title}</h3>
+      <div style={{ columns: 2 }}>
+
+        {teste.korSplit.map(string => <ul>
+          {string.split(' ').map(word => <><span style={{ background: '#efefef', marginRight: '10px' }}>
+            {word}{" "}
+          </span></>)}
+        </ul>)}
+
+        {teste.engSplit.map(string => <ul>
+          <li>
+            <li>{string}</li>
+
+          </li>
+        </ul>)}
+      </div></>}
+
   </div>;
 }
 
 export default Home;
+
+/* 
+
+   {data?.posts && data.posts.map(post => (<div key={post.link}>
+        <ul>
+          <li>title: {post.title}</li>
+          <li>kor: {post.kor}</li>
+          <li>eng: {post.eng}</li> 
+<li>korSplit: {post.korSplit.length}</li>
+ <li>korSplit: {post.korSplit.join("/")}</li> 
+<li>engSplit: {post.engSplit.length}</li>
+ <li>engSplit: {post.engSplit.join("/")}</li> 
+        </ul >
+  <hr />
+        </div >
+      ))} 
+      
+*/
